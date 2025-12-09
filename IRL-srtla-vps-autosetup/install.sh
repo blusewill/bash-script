5a82617cd5e1af8a8cfdbd79ddc1a5f0efeff7ab
blusewill <blusewillstudio@protonmail.com>
2025-12-09 19:43:40 +0800

    New Srtla Version

--- IRL-srtla-vps-autosetup/install.sh
+++ IRL-srtla-vps-autosetup/install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-printf "HI! $USER\n"
-printf "For some cases we might need your Account Password.\n"
-printf "Plaese Insert your account password "
-
-read -s -r "account_password"
-
 current_directory=$PWD
 
 if ! command -v docker >/dev/null 2>&1; then
@@ -21,10 +15,24 @@ if [ $docker_installed = 0 ]; then
   curl -fsSL https://get.docker.com -o $current_directory/install-docker.sh
   chmod +x $current_directory/install-docker.sh
 
-  echo $account_password | sh $current_directory/install-docker.sh
+  sudo sh $current_directory/install-docker.sh
 fi
 
-docker run -d --name belabox-receiver -p 5000:5000/udp -p 8181:8181/tcp -p 8282:8282/udp -p 3000:3000/tcp -v ./config.json:/app/config.json datagutt/belabox-receiver:latest
+printf "Please type your belabox username :"
+
+read -r "belabox_username"
+
+printf "\n Please type your belabox password : "
+
+read -s "belabox_password"
+
+curl -fsSL https://raw.githubusercontent.com/blusewill/bash-script/refs/heads/main/IRL-srtla-vps-autosetup/config.json -o config.json
+
+sed -i -e "s/belabox-user/$belabox_username" "$current_directory/config.json"
+
+sed -i -e "s/belabox-pass/$belabox_password" "$current_directory/config.json"
+
+docker run -d --name belabox-receiver -p 5000:5000/udp -p 8181:8181/tcp -p 8282:8282/udp -p 3000:3000/tcp -v $current_directory/config.json:/app/config.json datagutt/belabox-receiver:latest
 
 prinrtf "Now System will restart in 10 seconds..."
 sleep 10
