7ddd11ec6e6c6ce86e60b5c023cae5b2fb186c17
blusewill <blusewillstudio@protonmail.com>
2026-04-22 02:15:19 +0800

    Update Bing wallpaper

--- Bing_Wallpaper/get-wallpaper.sh
+++ Bing_Wallpaper/get-wallpaper.sh
@@ -1,14 +1,33 @@
-urlpath=$( \
-curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en_US" \
-| xmllint --xpath "/rss/channel/item/link/text()" - \
-| sed 's/1366x768/1920x1080/g' \
+#!/bin/bash
+
+SleepTime=0
+
+urlpath=$(
+  curl -s "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en_US" |
+    xmllint --xpath "/rss/channel/item/link/text()" - 2>/dev/null |
+    sed 's/1366x768/1920x1080/g'
 )
 
-if ping -q -c 1 -W 1 google.com >/dev/null 2>&1; then
-    rm -rf ~/Pictures/BingWallpaper/Daily.png
-    curl "https://www.bing.com$urlpath" -o ~/Pictures/BingWallpaper/Daily.png
+while true; do
+  if ping -q -c 1 -W 1 google.com >/dev/null 2>&1; then
+    if [ -n "$urlpath" ]; then
+      curl -s "https://www.bing.com$urlpath" -o ~/Pictures/BingWallpaper/Daily.png
+      feh --bg-fill ~/Pictures/BingWallpaper/Daily.png
+      notify-send "Bing Wallpaper" "Wallpaper updated successfully"
+    else
+      notify-send "Bing Wallpaper" "Failed to fetch image URL"
+    fi
+    break
+  else
     feh --bg-fill ~/Pictures/BingWallpaper/Daily.png
-else
-    feh --bg-fill ~/Pictures/BingWallpaper/Daily.png
-fi
 
+    if ((SleepTime < 600)); then
+      SleepTime=$((SleepTime + 60))
+      notify-send "Bing Wallpaper Script" "Retrying in $SleepTime seconds"
+      sleep "$SleepTime"
+    else
+      notify-send "Bing Wallpaper Script" "Wallpaper fetching failed after retries"
+      break
+    fi
+  fi
+done
